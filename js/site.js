b64e43b39843356894ef498b7e2457f9bfec6fd0
mateomaza <1mateomaza@gmail.com>
2022-06-23 12:51:34 -0500

    done!
    proud of the layout. and also i really liked my mental process through this, had a lot of clarity.

--- js/site.js
+++ js/site.js
@@ -1,8 +1,11 @@
 const btn = document.getElementById("calculate");
 const table = document.getElementById("display");
 
+
 // Controller
+
 function getValues() {
+
     let balance = document.getElementById("loanAmount").value;
     let term = document.getElementById("payments").value;
     let rate = document.getElementById("interestRate").value;
@@ -17,63 +20,94 @@ function getValues() {
             balance > 0 && term > 0 && rate > 0) {
 
             let data = generateData(balance, term, rate);
-            displayTable(data, term);
+            let payment = data[0];
+            let principalValues = data[1];
+            let interestValues = data[2];
+            let totalIntValues = data[3];
+            let balanceValues = data[4];
+            let principalSum = data[5];
+            let interestSum = data[6];
+
+            displayTable(term, payment, principalValues, interestValues, totalIntValues,
+                balanceValues, principalSum, interestSum);
+
         } else {
             alert("Values must be whole numbers higher than 0");
         }
     }
 }
 
+
+// Logic
+
 function generateData(b, t, r) {
 
     let data = []
-    
-    let payment = b * r / 1200 / (1 - (1 + r / 1200) ** -t).toFixed(2);
+
+    let payment = b * r / 1200 / (1 - (1 + r / 1200) ** -t);
     let principalValues = [];
     let interestValues = [];
     let totalIntValues = [];
     let balanceValues = [];
+    let principalSum = 0;
     let interestSum = 0;
 
     for (let i = 0; i < t; i++) {
-        let currentInterest = (b * r / 1200).toFixed(2);
-        let currentPrincipal = (payment - currentInterest).toFixed(2);
+        let currentInterest = b * r / 1200;
+        let currentPrincipal = payment - currentInterest;
         b -= currentPrincipal;
-        principalValues.push(currentPrincipal);
-        interestValues.push(currentInterest);
-        interestSum += parseFloat(currentInterest);
+        principalValues.push(currentPrincipal.toFixed(2));
+        interestValues.push(currentInterest.toFixed(2));
+        principalSum += currentPrincipal
+        interestSum += currentInterest;
         totalIntValues.push(interestSum.toFixed(2));
         balanceValues.push(b.toFixed(2));
     }
 
     // "payment" is the same every month, that's why it's different
-    data = [payment, principalValues, interestValues, totalIntValues, balanceValues]
-    
+    data = [payment, principalValues, interestValues, totalIntValues, balanceValues, principalSum, interestSum];
+
     return data
 }
 
-function displayTable(data, t) {
 
-    let tableRows = []
+// Display
 
-    let principalValues = data[1]
-    let interestValues = data [2]
-    let totalIntValues = data[3]
-    let balanceValues = data[4]
-    
+function displayTable(t, p, pV, iV, totalIV, bV, pS, iS) {
+
+    let info = document.getElementById("info");
+    let p1 = document.getElementById("p1");
+    let p2 = document.getElementById("p2");
+    let p3 = document.getElementById("p3");
+
+    let tableRows = [];
+
     for (let index = 0; index < t; index++) {
 
-        let pV = principalValues[index]
-        let iV = interestValues[index]
-        let totalIV = totalIntValues[index]
-        let bV = balanceValues[index]
+        let monthP = pV[index];
+        let monthI = iV[index];
+        let monthTotalI = totalIV[index];
+        let monthB = bV[index];
 
-        tableRows += `<tr><td>${index+1}</td><td>${data[0]}</td><td>${pV[index]}</td>
-        <td>${iV[index]}</td><td>${totalIV[index]}</td><td>${bV[index]}</td>`
+        tableRows += `<tr><td>${index+1}</td><td>$${p.toFixed(2)}</td><td>$${monthP}</td>
+        <td>$${monthI}</td><td>$${monthTotalI}</td><td>$${monthB}</td>`;
     }
 
-    table.innerHTML = tableRows 
+    table.innerHTML = tableRows;
+
+    info.innerHTML = `<p class="text-light fs-5 fw-light fst-italic text-decoration-underline">
+    Your Monthly Payments</p><p class="text-light fw-bold display-4">${p.toFixed(2)}</p>`;
+
+
+    p1.innerHTML = `Total Principal: $${pS.toFixed(2)}`;
+    p2.innerHTML = `Total Interest: $${iS.toFixed(2)}`;
+    p3.innerHTML = `Total Cost: $${(pS + iS).toFixed(2)}`;
+
+
 
 }
+
+
+// Event Handler
 
 btn.addEventListener("click", getValues)
